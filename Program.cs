18c49c2d5e854ae19804de19566aba0b34a8b75f
shemsAsf <137190111+shemsAsf@users.noreply.github.com>
2026-04-27 17:14:50 +0200

    Made sure DB migrate before trying to access the tables

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using PersonalApi.Auth;
@@ -42,6 +43,7 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
+    db.Database.Migrate();
     db.Database.EnsureCreated();
     db.GenerateFirstAdmin(ekaterinaSettings);
     db.SaveChanges();
